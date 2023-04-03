7e4fbc684f0c7ff4c1d305fb872eea2824c6205f
neerojstha <nirojstha@hotmail.com>
2023-04-03 20:12:53 +0000

    allowed hosts submitted

--- openbooking/settings.py
+++ openbooking/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['open-booking.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['open-booking.herokuapp.com', 'localhost']
 
 
 
